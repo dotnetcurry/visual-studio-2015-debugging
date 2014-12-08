a9237ff574fc36dd9fa3568ece7cb2972f552aa2
DotNetCurry <dotnetcurry@a2zknowledgevisuals.com>
2014-12-08 08:38:16 +0530

    projects added

---
+++ VisualStudio2015DebugDemo/VisualStudio2015DebugDemo/SalesCalculatorUI/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesCalculatorUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SalesLibrary.SalesCalculation calci = new SalesLibrary.SalesCalculation();
+            double netProfit = calci.SalesNetProfit(2000, 4000, 15000);
+            Console.WriteLine("Sales Net Profit is - $ {0}", netProfit);
+            List<SalesLibrary.Customer> customers = calci.FetchCustomer();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ VisualStudio2015DebugDemo/VisualStudio2015DebugDemo/SalesLibrary/SalesCalculation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesLibrary
+{
+    public class SalesCalculation
+    {
+        public double SalesNetProfit(double COGS, double Expenses, double ActualSales)
+        {
+            return ActualSales - (COGS + Expenses);
+        }
+        NorthwindDataContext dataContext = new NorthwindDataContext();
+        public List<Customer> FetchCustomer()
+        {
+            var customers = dataContext.Customers.ToList();
+            return customers;
+        }
+    }
+}
